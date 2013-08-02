ec74053ac175493f1308d2417f560200df956aaf
Nass <nasali@anypresence.com>
2013-08-02 12:18:25 -0400

    Added shared spec.

--- lib/dm-odata-adapter/odata_adapter.rb
+++ lib/dm-odata-adapter/odata_adapter.rb
@@ -2,6 +2,20 @@ module DataMapper
   module Adapters
     class OdataAdapter < DataMapper::Adapters::AbstractAdapter
       
+      def initialize(name, options)
+        super
+        @options = options
+        scheme = @options.fetch(:scheme)
+        host = @options.fetch(:host)
+        port = @options.fetch(:port,nil)
+        host = "#{host}:#{port}" if port
+        path = @options.fetch(:path)
+        service_url = "#{scheme}://#{host}#{path}" 
+        DataMapper.logger.debug("Connecting using #{service_url}")
+        @service = OData::Service.new(service_url)
+        DataMapper.logger.debug("Initialized service to #{@service.inspect}")
+      end
+      
     end
   end
 end
