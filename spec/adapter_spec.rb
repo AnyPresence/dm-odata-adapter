be773cc1120c4d346aeb3e4f2efd4f7ec60b4f1b
NA <nasali@anypresence.com>
2013-08-05 09:19:25 -0600

    Explicitly specify activesupport gem version dependency.

--- spec/adapter_spec.rb
+++ spec/adapter_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe DataMapper::Adapters::OdataAdapter do
   
   before :all do
-   @adapter = DataMapper.setup(:default, :adapter => 'odata', :scheme => 'http', :host => 'localhost', :port => '8181', :path => 'odata'
+   @adapter = DataMapper.setup(:default, :adapter => 'odata', :scheme => 'http', :host => 'localhost', :port => '8181', :path => '/sqlconnector/data/conn/live.rsc/dbo.heffalumps'
    )      
   end
   
