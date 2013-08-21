f2488ac0cac0c1f30b805586e3868e6eff896566
Nass <nasali@anypresence.com>
2013-08-21 16:17:51 -0400

    Refactored transformer to always build translation classes using all properties of a model.

--- lib/dm-odata-adapter/transformer.rb
+++ lib/dm-odata-adapter/transformer.rb
@@ -12,17 +12,18 @@ module DataMapper
           klass_name = build_class_name(model)
           klass = @processed_classes[klass_name]
           if klass.nil?
-	          attributes = [*hash.keys.select{|k| k.to_sym}]
+            all_properties = make_field_to_property_hash(model)
+	          attributes = [*all_properties.keys.select{|k| k.to_sym}]
             klass = Container.const_set(klass_name.to_sym,Object.class)
 	          klass.class_eval do
   	          attr_accessor *attributes
             end
             DataMapper.logger.debug("Caching new class for model #{model}")
-            @processed_classes[klass_name] = klass
+            @processed_classes[klass_name] = klass.new
           else
-            DataMapper.logger.debug("Cache has class for model #{model}")
+            DataMapper.logger.debug("Cache has instance for model #{model}")
           end
-          instance = to_ruby_odata_instance(klass.new, hash)
+          instance = to_ruby_odata_instance(Container.const_get(klass_name).new, hash)
           DataMapper.logger.debug("Returning instance #{instance.instance_variables}")
           instance
         end
@@ -71,6 +72,7 @@ module DataMapper
 	      end
 	      
 	      def to_ruby_odata_instance(instance, hash)
+	        DataMapper.logger.debug("to_ruby_odata_instance called with #{instance.instance_variables} and #{hash.inspect}")
 	        hash.each do |field, value|
 	          DataMapper.logger.debug("Setting #{field} = #{value}")
             instance.send("#{field}=",value)
